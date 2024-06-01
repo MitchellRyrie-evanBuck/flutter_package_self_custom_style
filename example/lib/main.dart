01ae54d45af84a955812d292292286f7223b9255
liuxiaowen <liuxiaowen66621@gmail.com>
2024-06-02 01:22:52 +0800

    feat:  github

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter package',
+      title: 'GitHub Contributions Grid',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 255, 255, 255)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,15 +35,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            StyleWidget(),
+            Container(
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: StyleWidget(),
+                )),
           ],
         ),
       ),
--- lib/flutter_package_self_custom_style.dart
+++ lib/flutter_package_self_custom_style.dart
@@ -9,23 +9,58 @@ class StyleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    late double screenWidth = MediaQuery.of(context).size.width - 60;
+    late double screenWidth = MediaQuery.of(context).size.width - 80;
 
     return Column(
       children: [
         Container(
-          alignment: Alignment.center,
-          child: Text(
-            'Github',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-        ),
-        Container(
           width: screenWidth,
-          height: 140,
-          color: Colors.black12,
+          height: 200,
+          color: Colors.white,
+          child: ContributionsScreen(),
         )
       ],
     );
+  }
+}
+
+class ContributionsScreen extends StatelessWidget {
+  final List<int> contributions =
+      List.generate(112, (index) => index % 7); // 模拟的提交数据
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 16, // 一周 7 天
+          crossAxisSpacing: 3.0,
+          mainAxisSpacing: 3.0,
+        ),
+        itemCount: contributions.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              color: getColorForContribution(contributions[index]),
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+          );
+        });
+  }
+
+  Color getColorForContribution(int contribution) {
+    switch (contribution) {
+      case 0:
+        return Colors.lightGreen[100]!;
+      case 1:
+        return Colors.lightGreen[200]!;
+      case 2:
+        return Colors.lightGreen[400]!;
+      case 3:
+        return Colors.green[500]!;
+      case 4:
+        return Colors.green[700]!;
+      default:
+        return const Color.fromARGB(255, 222, 222, 222);
+    }
   }
 }
